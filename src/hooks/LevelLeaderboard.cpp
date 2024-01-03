5a2ae5a2a3b186acc33fe2cf8dd23963510ab5db
Cvolton <cvolton@cvolton.eu>
2024-01-03 03:52:46 +0100

    update LevelLeaderboard to use layouts

--- src/hooks/LevelLeaderboard.cpp
+++ src/hooks/LevelLeaderboard.cpp
@@ -31,16 +31,23 @@ class $modify(BILevelLeaderboard, LevelLeaderboard) {
     bool init(GJGameLevel* level, LevelLeaderboardType type, LevelLeaderboardMode mode) {
         if(!LevelLeaderboard::init(level, type, mode)) return false;
 
-        auto buttonSprite = ButtonSprite::create((level->m_dailyID ? "Daily" : "Normal"), 20, true, "bigFont.fnt", "GJ_button_01.png", 30, 0.5);
-        auto buttonButton = CCMenuItemSpriteExtra::create(
-            buttonSprite,
-            this,
-            menu_selector(BILevelLeaderboard::onLeaderboardDaily)
-        );
-        buttonButton->setSizeMult(1.2f);
-        buttonButton->setPosition({196,-87});
-        m_buttonMenu->addChild(buttonButton);
-        buttonButton->setID("daily-button"_spr);
+        //daily btn disabled for editor levels cuz it makes no sense there AND for platformer levels cuz it's not implemented server-side
+        if(m_level->m_levelType != GJLevelType::Editor && !m_level->isPlatformer()) {
+
+            if(auto secondMenu = m_mainLayer->getChildByID("type-menu")) {
+                auto buttonSprite = ButtonSprite::create((level->m_dailyID ? "Daily" : "Normal"), 20, true, "bigFont.fnt", "GJ_button_01.png", 30, 0.5);
+                auto buttonButton = CCMenuItemSpriteExtra::create(
+                    buttonSprite,
+                    this,
+                    menu_selector(BILevelLeaderboard::onLeaderboardDaily)
+                );
+                buttonButton->setID("daily-button"_spr);
+
+                secondMenu->addChild(buttonButton);
+                secondMenu->updateLayout();
+            }
+            
+        }
 
         return true;
     }
