37b3badbeffb24cb01153dd47d1174ce77f7ee5e
Cvolton <cvolton@cvolton.eu>
2024-01-13 10:10:45 +0100

    change platformer (i) position

--- src/hooks/LevelAreaInnerLayer.cpp
+++ src/hooks/LevelAreaInnerLayer.cpp
@@ -27,7 +27,8 @@ class $modify(BILevelAreaInnerLayer, LevelAreaInnerLayer) {
     */
     void addInfoBtn(int levelID, CCPoint position) {
         auto sprite = CCSprite::createWithSpriteFrameName("GJ_infoIcon_001.png");
-        sprite->setScale(0.6f);
+        //sprite->setScale(0.6f);
+        sprite->setScale(0.5f);
         auto btn = CCMenuItemSpriteExtra::create(
             sprite,
             this,
@@ -49,13 +50,25 @@ class $modify(BILevelAreaInnerLayer, LevelAreaInnerLayer) {
         m_fields->m_menu = CCMenu::create();
         m_fields->m_menu->setPosition({0, 0});
         m_fields->m_menu->setZOrder(10);
+        m_fields->m_menu->setID("info-menu"_spr);
         static_cast<CCNode*>(getChildren()->objectAtIndex(getChildrenCount() - 1))->addChild(m_fields->m_menu);
 
-        addInfoBtn(5001, {-58, -79});
-        addInfoBtn(5002, {138, -22});
-        addInfoBtn(5003, {-61, 35});
-        addInfoBtn(5004, {93, 89});
-        
+        /*addInfoBtn(5001, {-58, -106});
+        //addInfoBtn(5002, {139, -50});
+        addInfoBtn(5002, {68, -51});
+        addInfoBtn(5003, {-58, 5});
+        //addInfoBtn(5004, {94, 59});
+        addInfoBtn(5004, {22, 58});*/
+
+        /*addInfoBtn(5001, {-95.5f, -124});
+        addInfoBtn(5002, {102.5f, -70});
+        addInfoBtn(5003, {-95.5f, -13});
+        addInfoBtn(5004, {57.5f, 41});*/
+
+        addInfoBtn(5001, {-60, -108.5f});
+        addInfoBtn(5002, {68, -52.5f});
+        addInfoBtn(5003, {-60, 2});
+        addInfoBtn(5004, {57.5f, 41});
 
         return true;
     }
