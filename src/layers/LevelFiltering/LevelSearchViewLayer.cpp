0a3ad7fe47f3c90020d97f20c211daa3cc29192e
Cvolton <cvolton@cvolton.eu>
2024-02-29 23:14:56 +0100

    fix lsvl incorrectly rate limiting

--- src/layers/LevelFiltering/LevelSearchViewLayer.cpp
+++ src/layers/LevelFiltering/LevelSearchViewLayer.cpp
@@ -161,13 +161,13 @@ void LevelSearchViewLayer::startLoading(){
         searchObj->retain();
         // this amounts to 80 requests per second, which is 20 below the server limit
         auto time = 0.75 - (TimeUtils::getFullDoubleTime() - m_lastLoadTime);
-        log::debug("Time: {}", time);
         if(time < 0) {
+            time = 0;
             this->queueLoad(0);
         } else {
             this->getScheduler()->scheduleSelector(schedule_selector(LevelSearchViewLayer::queueLoad), this, 1, 0, time, false);
         }
-        m_lastLoadTime = TimeUtils::getFullDoubleTime();
+        m_lastLoadTime = TimeUtils::getFullDoubleTime() + time;
     }
 }
 
