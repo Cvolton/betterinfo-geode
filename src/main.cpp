f94315df96b1bc976a38bc38b600e4dc27ea69dd
Cvolton <cvolton@cvolton.eu>
2026-03-05 21:44:57 +0100

    remove old code for removing lists created by mac bug
    no way affected people havent updated until this point

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,6 @@
 #include <Geode/modify/MenuLayer.hpp>
 #include <Geode/modify/GameManager.hpp>
 
-#include <thread>
-
 #include "managers/BetterInfoStats.h"
 #include "managers/BetterInfoCache.h"
 #include "utils.hpp"
@@ -21,41 +19,6 @@ void BI_DLL finishLoadingManagers() {
     BetterInfoStats::sharedState()->finishLoading();
 }
 
-void BI_DLL fixLevelLists() {
-    log::info("Deleting level lists created by Mac bug");
-
-    auto LLM = LocalLevelManager::sharedState();
-    auto lists = CCArrayExt<GJLevelList*>(LLM->m_localLists);
-
-    std::vector<GJLevelList*> blanks;
-
-    for(auto list : lists) {
-        if(list->m_listID == 0 && list->m_listName.empty() && (list->m_levels.size() == 0) && list->m_levelsString.empty()) {
-            blanks.push_back(list);
-        }
-    }
-
-    if(blanks.size() > 10) {
-        for(auto list : blanks) {
-            LLM->m_localLists->removeObject(list);
-        }
-    }
-
-    log::info("Deleting level lists semi-favorited by incorrectly expecting 2.200 behavior");
-    auto GLM = GameLevelManager::sharedState();
-
-    std::vector<gd::string> toRemove;
-
-    auto lists2 = CCDictionaryExt<gd::string, GJLevelList*>(GLM->m_favoriteLists);
-    for(auto [key,value] : lists2) {
-        if(!value->m_favorite) toRemove.push_back(key);
-    }
-
-    for(auto key : toRemove) {
-        GLM->m_favoriteLists->removeObjectForKey(key);
-    }
-}
-
 class BI_DLL $modify(MenuLayer) {
     bool init() {
         if(!MenuLayer::init()) return false;
@@ -66,7 +29,6 @@ class BI_DLL $modify(MenuLayer) {
         if(!managersLoaded) {
             managersLoaded = true;
             finishLoadingManagers();
-            fixLevelLists();
         }
 
         return true;
