d29759c0c48df0831c1ec82c13c235d25d37acc3
Cvolton <cvolton@cvolton.eu>
2026-03-07 21:49:17 +0100

    enable server api support

--- src/utils/ServerUtils.cpp
+++ src/utils/ServerUtils.cpp
@@ -1,6 +1,6 @@
 #include "ServerUtils.h"
 #include "../utils.hpp"
-//#include "../integrations/ServerAPIEvents.hpp"
+#include <km7dev.server_api/include/ServerAPIEvents.hpp>
 
 #include <asp/iter.hpp>
 
@@ -17,20 +17,19 @@ web::WebRequest ServerUtils::getBaseRequest(bool setUserAgent) {
 }
 
 std::string ServerUtils::getBaseURL() {
-    /*if(Loader::get()->isModLoaded("km7dev.server_api")) {
+    if(Loader::get()->isModLoaded("km7dev.server_api")) {
         auto url = ServerAPIEvents::getCurrentServer().url;
         if(!url.empty() && url != "NONE_REGISTERED") {
             while(url.ends_with("/")) url.pop_back();
             return url;
         }
-    }*/
+    }
 
     // The addresses are pointing to "https://www.boomlings.com/database/getGJLevels21.php"
     // in the main game executable
     char* originalUrl = nullptr;
     #ifdef GEODE_IS_WINDOWS
         static_assert(GEODE_COMP_GD_VERSION == 22081, "Unsupported GD version");
-        // origia
         originalUrl = (char*)(base::get() + 0x558b70);
     #elif defined(GEODE_IS_ARM_MAC)
         static_assert(GEODE_COMP_GD_VERSION == 22081, "Unsupported GD version");
